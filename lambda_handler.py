7fa54fff45de6f6f98f2470587e4afe55a7e2efb
rafael-torres-nantes <rafatorresnantes@gmail.com>
2025-02-04 17:28:29 -0400

    refactor: update lambda_handler to use BedrockInference and improve logging

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,41 +1,39 @@
 import json
-from bedrock_models.claude_model import BedrockInference
-from storage_data.s3_bucket import S3BucketClass
+from bedrock_models.bedrock_inference import BedrockInference
 from templates.prompts_template import create_system_prompt, create_user_prompt
 
 def lambda_handler(event, context):
-    # Inicializa a classe BedrockInference
+    """
+    Função Lambda que invoca o modelo Bedrock para responder a uma pergunta do usuário.
+
+    Args:
+        event (dict): Dicionário com os dados de entrada da função Lambda.
+        context (object): Objeto de contexto da função Lambda.
+    """
+    # 1- Imprime o evento recebido
+    print(f'[DEBUG] Evento recebido: {event}')
+    
+    # 2 - Inicializa a classe BedrockInference
     bedrock_inference = BedrockInference()
     
-    # Inicializa a classe S3BucketClass
-    s3_bucket = S3BucketClass()
-    
-    # Extrai o prompt do evento
+    # 3 - Extrai o prompt do evento
     user_query = event.get('user_query', '')
     image_path = event.get('image_path', '')
-    
-    # Gera o prompt do sistema e do usuário
+    print(f'[DEBUG] Pergunta do usuário: {user_query}')
+
+    # 4 - Gera o prompt do sistema e do usuário
     system_prompt = create_system_prompt()
     user_prompt = create_user_prompt(user_query)
+    print(f'[DEBUG] Prompt do sistema: {system_prompt}')
+      
+    # 5 - Invoca o modelo Bedrock com o prompt combinado
+    response_text = bedrock_inference.invoke_model(system_prompt, user_prompt, image_path)
+    print(f'[DEBUG] Resposta gerada: {response_text}')
     
-    # Combina os prompts
-    combined_prompt = f"{system_prompt}\n{user_prompt}"
-    
-    # Invoca o modelo Bedrock com o prompt combinado
-    response_text = bedrock_inference.invoke_model(combined_prompt, image_path)
-    
-    # Define o nome do bucket e do arquivo no S3
-    bucket_name = 'your-s3-bucket-name'
-    s3_filename = 'response.json'
-    
-    # Exporta a resposta para o S3
-    s3_bucket.export_dynamodb_to_s3([response_text], bucket_name, s3_filename)
-    
-    # Retorna a resposta como JSON
+    # 6 - Retorna a resposta como JSON
     return {
         'statusCode': 200,
-        'body': json.dumps({
-            'response': response_text,
-            's3_location': f"s3://{bucket_name}/{s3_filename}"
-        })
+        'body': json.dumps({ 'response': response_text})
     }
+
+lambda_handler({'user_query': 'Oque é esse documento?', 'image_path': './image/teste.jpg'}, None)
