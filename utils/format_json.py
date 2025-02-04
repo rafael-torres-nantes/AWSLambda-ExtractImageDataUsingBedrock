df3cbf37eac05c1bac785b36665fa79f733c0df9
rafael-torres-nantes <rafatorresnantes@gmail.com>
2025-02-04 17:32:56 -0400

    refactor: enhance docstring in write_jsonl function for clarity and completeness

--- utils/format_json.py
+++ utils/format_json.py
@@ -1,6 +1,13 @@
 import json
 
 def write_jsonl(data, file_path) -> None:
+    """
+    Função que escreve uma lista de dicionários em um arquivo JSONL.
+
+    Args:
+        data (list): Lista de dicionários a ser escrita.
+        file_path (str): Caminho do arquivo a ser escrito.
+    """
     with open(file_path, "w") as file:
         for item in data:
             json_str = json.dumps(item)
